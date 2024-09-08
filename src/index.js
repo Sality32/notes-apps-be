6a71580cc0f90f2f6cf6402350331f07e2d4ef42
Yoga Subagja <42853517+Sality32@users.noreply.github.com>
2024-09-08 21:20:52 +0700

    add example api with params (#3)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/:name', (req, res) => {
+  res.send(`Hallo ${req.params.name}`);
+});
+
+
 
 app.listen(port, () => {
   console.log(`App listeing on port ${port}`);
